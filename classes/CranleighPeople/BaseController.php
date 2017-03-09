97496f98ab0c54be4fedb5307610cbf13708e105
Fred Bradley <hello@fredbradley.co.uk>
2017-03-09 17:21:09 +0000

    Move settings class instance outside of base controller class so that it only loads once.

--- classes/CranleighPeople/BaseController.php
+++ classes/CranleighPeople/BaseController.php
@@ -1,29 +1,30 @@
 <?php
 namespace CranleighPeople;
 
+new Settings();
+
 class BaseController {
 
 	function __construct() {
-		new Settings();
-		
-		$this->settings = get_option('cran_people_basic');	
+
+		$this->settings = get_option('cran_people_basic');
 
 	}
-	
+
 	function setting($variable) {
 		return $this->settings[$variable];
 	}
-	
+
 	function restore_current_blog() {
 		if (is_multisite()):
 			return restore_current_blog();
 		endif;
 	}
-	
+
 	function switch_to_blog($new_blog) {
 		if (is_multisite()):
 			return switch_to_blog($new_blog);
 		endif;
 	}
-	
+
 }
