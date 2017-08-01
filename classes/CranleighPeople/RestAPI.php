f9ae1cf16da371f58828e8b320b69d742b2c3cd7
Fred Bradley <hello@fredbradley.co.uk>
2017-08-01 12:09:53 +0100

    Add REST API Endpoint

--- classes/CranleighPeople/RestAPI.php
+++ classes/CranleighPeople/RestAPI.php
@@ -8,8 +8,51 @@ class RestAPI {
 
 	public function __construct() {
 		add_action( 'rest_api_init', array($this,'wpshout_register_routes') );
+		add_action( 'rest_api_init', array($this, 'listStaff'));
+
+		add_filter( 'rest_query_vars', array($this,'my_allow_meta_query' ));
 	}
 
+	public function my_allow_meta_query( $valid_vars ) {
+
+		$valid_vars = array_merge( $valid_vars, array( 'meta_key', 'meta_value' ) );
+		return $valid_vars;
+	}
+
+	public function listStaff() {
+		$args = [
+			"posts_per_page" => -1,
+			"post_type" => "staff"
+		];
+
+		if (isset($_GET['username'])) {
+			$args["meta_query"] = [[
+				"key" => "staff_username",
+				"value" => $_GET['username']
+			]];
+		}
+		$query = new WP_Query($args);
+		$output = [];
+
+		while($query->have_posts()): $query->the_post();
+			$person = new \stdClass();
+			$person->username = get_post_meta(get_the_ID(), 'staff_username', true);
+			$photoAttachmentID = get_post_thumbnail_id();
+			$person->imageHTML = wp_get_attachment_image($photoAttachmentID, "staff-profile");
+			$person->name = get_the_title();
+			$person->jobTitle = get_post_meta(get_the_ID(), 'staff_leadjobtitle',true);
+			$person->ID = get_the_ID();
+			$output[$person->username] = $person;
+		endwhile;
+		wp_reset_query();
+		wp_reset_postdata();
+
+		if ($query->post_count !== 1) {
+			return $output;
+		} else {
+			return $person;
+		}
+	}
 	public function wpshout_register_routes() {
 	    register_rest_route(
 	        'people',
@@ -19,6 +62,24 @@ class RestAPI {
 	            'callback' => array($this,'photos'),
 	        )
 	    );
+
+		register_rest_route(
+			'people',
+			'list',
+			array(
+				'methods' => 'GET',
+				'callback' => array($this, 'listStaff')
+			)
+		);
+
+		register_rest_route(
+			'people',
+			'/staff/(?P<id>\d+)',
+			array(
+				'methods' => 'GET',
+				'callback' => array($this, 'listStaff')
+			)
+		);
 	}
 	public function photos() {
 		global $wpdb;
