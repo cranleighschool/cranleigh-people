320d9c1b931e5745181c471b177d7c9e50c0a82a
Fred Bradley <hello@fredbradley.co.uk>
2017-11-03 11:28:22 +0000

    Update Version Number

--- cranleigh_people.php
+++ cranleigh_people.php
@@ -4,7 +4,7 @@
 	Plugin URI: http://www.cranleigh.org
 	Description: One plugin that controls the people who work at Cranleigh.
 	Author: Fred Bradley
-	Version: 1.5.6
+	Version: 1.5.7
 	Author URI: http://fred.im
 */
 
