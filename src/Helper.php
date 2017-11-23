223d876fa8c7edce8f5274c3e857b38141f0341d
Fred Bradley <hello@fredbradley.co.uk>
2017-11-23 12:42:17 +0000

    Refactored how phone numbers are santizied

--- src/Helper.php
+++ src/Helper.php
@@ -8,20 +8,85 @@
 
 namespace CranleighSchool\CranleighPeople;
 
+use Exception;
 
 class Helper {
 
-	public static function santitizePhoneHref( $number ) {
+	public static $phoneNumber;
 
-		if ( substr( $number, 0, 1 ) == "+" ) {
+	public static function santitizePhoneHref( string $number ) {
+
+
+		if ( self::phoneStartsWith("+", $number) ) {
 			return $number;
 		} else {
-			$str = str_replace( "01483", "+441483", $number );
-			$str = str_replace( " ", "", $str );
-
-			return $str;
+			$number = preg_replace('/\D/', '', $number);
+			return "+44".substr($number, 1);
 		}
 
+	}
+
+	public static function sanitizePhoneDisplay( string $number ) {
+		$number = preg_replace('/\D/', '', $number);
+
+		self::$phoneNumber = $number;
+
+		if ( self::phoneStartsWith(44) ) {
+			$number = "0".substr($number,2);
+			self::$phoneNumber = $number;
+
+		}
+		if ( !self::phoneStartsWith("0")) {
+			$number = "0".$number;
+			self::$phoneNumber = $number;
+
+		}
+
+		if (strlen($number)==11) {
+			if ( self::phoneStartsWith( "01" ) || self::phoneStartsWith( "07" ) ) {
+				return preg_replace( "/([0-9]{5})([0-9]{6})/", "$1 $2", $number );
+			} elseif ( self::phoneStartsWith( "02" ) ) {
+				// EG: 020 8811 8181
+				return preg_replace( "/([0-9]{3})([0-9]{4})([0-9]{4})/", "$1 $2 $3", $number );
+			} elseif ( self::phoneStartsWith( "08" ) || self::phoneStartsWith( "09" ) ) {
+				return preg_replace( "/([0-9]{4})([0-9]{3})([0-9]{4})/", "$1 $2 $3", $number );
+			}
+		} elseif (strlen($number) == 10) {
+			if ( self::phoneStartsWith(["0500", "0800"])) {
+				return preg_replace("/([0-9]{4})([0-9]{6})/", "$1 $2", $number);
+			}
+		} elseif (strlen($number)==8 && self::phoneStartsWith("0800")) {
+			return preg_replace("/([0-9]{4})([0-9]{4})/", "$1 $2", $number);
+		}
+
+		return $number;
+	}
+
+	public static function phoneStartsWith($startsWith, $phoneNumber=null) {
+
+		if ($phoneNumber===null) {
+			$phoneNumber = self::$phoneNumber;
+		}
+
+		if (is_array($startsWith)) {
+			$matchLength = strlen($startsWith[0]);
+			foreach ($startsWith as $item) {
+				if (strlen($item) !== $matchLength) {
+					throw new Exception("String lengths to check against are differing lengths. Trying to match: ".print_r($startsWith, true));
+					break;
+				}
+			}
+		}
+
+		if (is_array($startsWith)) {
+			if (in_array(substr($phoneNumber, 0, $matchLength), $startsWith)) {
+				return true;
+			}
+		} elseif (substr($phoneNumber, 0, strlen($startsWith)) == $startsWith) {
+			return true;
+		}
+
+		return false;
 	}
 
 	public static function santitizePositions( $positions, $not = null ) {
