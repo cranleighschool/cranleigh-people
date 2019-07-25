68ca1d2a97ce4d157a78999c047f97ab4b0fc10e
Fred Bradley <hello@fredbradley.co.uk>
2019-07-25 10:02:24 +0100

    I have realised that the settings page has to be outside of the CPT, due to CPT menu not appearing on houses sub sites

--- src/Settings.php
+++ src/Settings.php
@@ -33,7 +33,7 @@
 
 			function admin_menu()
 			{
-				add_submenu_page('edit.php?post_type='.Plugin::POST_TYPE_KEY, "Cranleigh People Settings", "Settings", "manage_options", "cranleigh_people_settings", array($this, 'plugin_page'));
+				add_options_page('Cranleigh People', 'Cranleigh People', 'manage_options', 'cranleigh_people_settings', array($this, 'plugin_page'));
 			}
 
 			function get_settings_sections()
