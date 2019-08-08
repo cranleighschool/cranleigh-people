07a7dd6c1c75b740404883e22d69a750d1f59908
Fred Bradley <hello@fredbradley.co.uk>
2019-08-08 08:56:18 +0100

    Write slack warnings on exception

--- src/Exceptions/StaffNotFoundException.php
+++ src/Exceptions/StaffNotFoundException.php
@@ -2,7 +2,17 @@
 
 	namespace CranleighSchool\CranleighPeople\Exceptions;
 
+	use CranleighSchool\CranleighPeople\Slacker;
+	use Throwable;
+
 	class StaffNotFoundException extends \Exception
 	{
+
+		public function __construct($message = "", $code = 0, Throwable $previous = NULL)
+		{
+			$slacker = new Slacker();
+			$slacker->post($message);
+			parent::__construct($message, $code, $previous);
+		}
 
 	}
--- src/Exceptions/TooManyStaffFound.php
+++ src/Exceptions/TooManyStaffFound.php
@@ -4,7 +4,15 @@
 	namespace CranleighSchool\CranleighPeople\Exceptions;
 
 
+	use CranleighSchool\CranleighPeople\Slacker;
+	use Throwable;
+
 	class TooManyStaffFound extends \Exception
 	{
-
+		public function __construct($message = "", $code = 0, Throwable $previous = NULL)
+		{
+			$slacker = new Slacker();
+			$slacker->post($message);
+			parent::__construct($message, $code, $previous);
+		}
 	}
