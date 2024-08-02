fd2ded4f52cbac9bf8881c192e5f4c564bdcc92f
fredbradley <fredbradley@users.noreply.github.com>
2024-08-02 14:23:29 +0000

    Apply version bump change

--- cranleigh-people.php
+++ cranleigh-people.php
@@ -4,7 +4,7 @@ Plugin Name: Cranleigh People
 Plugin URI: http://www.cranleigh.org
 Description: One plugin that controls the people who work at Cranleigh.
 Author: Fred Bradley
-Version: 3.0.0
+Version: 2.0.21
 Author URI: http://fred.im
 */
 
