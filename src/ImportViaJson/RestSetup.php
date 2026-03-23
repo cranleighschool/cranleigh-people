ddb78e572193c7c3a8da2b2c3db3d3817aef25d5
Fred Bradley <code@fredbradley.co.uk>
2026-03-23 14:00:05 +0000

    Update RestSetup.php
    Allow hyphens in username/initials.

--- src/ImportViaJson/RestSetup.php
+++ src/ImportViaJson/RestSetup.php
@@ -29,7 +29,7 @@ class RestSetup
 
         register_rest_route(
             'people',
-            'photo/(?P<initials>[a-zA-Z]{2}[a-zA-Z0-9.]*)', // Allows for people like TS2. This is the regex for initials.
+            'photo/(?P<initials>[a-zA-Z]{2}[a-zA-Z0-9.\-]*)', // Allows for people like TS2. This is the regex for initials.
             array(
                 'methods' => \WP_REST_Server::CREATABLE,
                 'callback' => new ImportPhotoRoute(),
@@ -52,7 +52,7 @@ class RestSetup
 
         register_rest_route(
             'people',
-            'remove/(?P<initials>[a-zA-Z]{2}[a-zA-Z0-9.]*)', // Allows for people like TS2. This is the regex for initials.
+            'remove/(?P<initials>[a-zA-Z]{2}[a-zA-Z0-9.\-]*)', // Allows for people like TS2. This is the regex for initials.
             array(
                 'methods' => \WP_REST_Server::DELETABLE,
                 'callback' => new RemovePerson(),
